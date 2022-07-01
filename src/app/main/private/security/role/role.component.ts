1775e658e9bd3bd5ffd55bc241a43905fbf75911
Rogger Ortiz <rogger.ortiz.br@gmail.com>
2022-06-30 23:51:25 -0500

    agregar iconos a roles

--- src/app/main/private/security/role/role.component.ts
+++ src/app/main/private/security/role/role.component.ts
@@ -6,48 +6,48 @@ import { SecurityService } from 'app/core/services/security.service';
 import { timeStamp } from 'console';
 
 @Component({
-  selector: 'app-role',
-  templateUrl: './role.component.html',
-  styleUrls: ['./role.component.scss']
+   selector: 'app-role',
+   templateUrl: './role.component.html',
+   styleUrls: ['./role.component.scss']
 })
 export class RoleComponent implements OnInit {
 
-  role: RoleDto = new RoleDto();
-  title: string = "Crear Rol";
+   role: RoleDto = new RoleDto();
+   title: string = "Crear Rol";
 
-  constructor(
-    private _securityService: SecurityService,
-    private _activeModal: NgbActiveModal
-  ) { }
+   constructor(
+      private _securityService: SecurityService,
+      private _activeModal: NgbActiveModal
+   ) { }
 
-  ngOnInit(): void {
-    if (this.role.id > 0)
-      this.title = "Editar Rol";
-  }
+   ngOnInit(): void {
+      if (this.role.id > 0)
+         this.title = "Editar Rol";
+   }
 
-  saveRole(): void {
-    // validations
-    if ((this.role.id ?? 0) == 0) {
-      this.role.status = 1;
-      this.role.reportingStatus = 1;
-      this._securityService.createRole(this.role).subscribe(apiResponseDto => {
-        if (apiResponseDto.type == 1) {
-          this.role = apiResponseDto.data;
-          this._activeModal.close({ success: true, role: this.role });
-        }
-      });
-    }
-    else {
-      this._securityService.editRole(this.role).subscribe(apiResponseDto => {
-        if (apiResponseDto.type == 1) {
-          this.role = apiResponseDto.data;
-          this._activeModal.close({ success: true, role: this.role });
-        }
-      });
-    }
-  }
+   saveRole(): void {
+      // validations
+      if ((this.role.id ?? 0) == 0) {
+         this.role.status = 1;
+         this.role.reportingStatus = 1;
+         this._securityService.createRole(this.role).subscribe(apiResponseDto => {
+            if (apiResponseDto.type == 1) {
+               this.role = apiResponseDto.data;
+               this._activeModal.close({ success: true, role: this.role });
+            }
+         });
+      }
+      else {
+         this._securityService.editRole(this.role).subscribe(apiResponseDto => {
+            if (apiResponseDto.type == 1) {
+               this.role = apiResponseDto.data;
+               this._activeModal.close({ success: true, role: this.role });
+            }
+         });
+      }
+   }
 
-  close(): void {
-    this._activeModal.close({ success: false });
-  }
+   close(): void {
+      this._activeModal.close({ success: false });
+   }
 }
